954b4f3016bbd59145cf6fec7149fa442dfdafe9
KatieGott <Katie.Gott@target.com>
2022-02-21 17:25:21 -0600

    exercise to make a functional component

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-function MyApp() {
+function MyInfo() {
     return (
+        <div>
+            <h1>Katie</h1>
+            <p>Hackbright grad learning React</p>
         <ul>
-            <li>First Item</li>
-            <li>Second Item</li>
-            <li>Third Item</li>
+            <li>1. Greece</li>
+            <li>2. St. Lucia</li>
+            <li>3. Ireland</li>
         </ul>
+        </div>
     )
 }
 
 ReactDOM.render(
-    <MyApp />, 
+    <MyInfo />, 
     document.getElementById("root")
 )
