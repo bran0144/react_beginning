cd4ff8b0e22f51679bea148c3c1a0a8396656c99
KatieGott <Katie.Gott@target.com>
2022-02-22 15:11:43 -0600

    changing styles based on if/else clauses

--- App.js
+++ App.js
@@ -7,18 +7,24 @@ function App() {
     const date = new Date()
     const hours = date.getHours()
     let timeOfDay 
+    const styles = {
+        fontSize: "30px"}
 
     if (hours < 12) {
         timeOfDay = "morning"
+        styles.color = "#04756F"
     } else if (hours >= 12 && hours < 17) {
         timeOfDay = "afternoon"
+        styles.color = "#2E0927"
     }else {
         timeOfDay = "night"
+        styles.color = "#D90000"
     }
 
-
     return (
-        <h1>Good {timeOfDay}!</h1>
+        <h1 style={styles}>Good {timeOfDay}!</h1>
+        // need to wrap in curly braces for the object and again for using JS
+        // can't use background-color - needs to be camelCase
     )
 }
 export default App
