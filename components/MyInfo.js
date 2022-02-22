1cf4c6e0757336c7a7a6dad584a3032cf239fd1a
KatieGott <Katie.Gott@target.com>
2022-02-22 09:58:20 -0600

    starting to use components and abstraction

---
+++ components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>Katie</h1>
+            <p>Hackbright grad learning React</p>
+        <ul>
+            <li>1. Greece</li>
+            <li>2. St. Lucia</li>
+            <li>3. Ireland</li>
+        </ul>
+        </div>
+    )
+}
+
+export default MyInfo
--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
-
-function MyInfo() {
-    return (
-        <div>
-            <h1>Katie</h1>
-            <p>Hackbright grad learning React</p>
-        <ul>
-            <li>1. Greece</li>
-            <li>2. St. Lucia</li>
-            <li>3. Ireland</li>
-        </ul>
-        </div>
-    )
-}
+import App from "./components/MyInfo"
 
 ReactDOM.render(
-    <MyInfo />, 
+    <App />, 
     document.getElementById("root")
 )
