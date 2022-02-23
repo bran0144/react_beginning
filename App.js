b81e53c96d13e4c7cb8148694f60f3499c6e1c6c
KatieGott <Katie.Gott@target.com>
2022-02-23 10:53:14 -0600

    creating a to do list

--- App.js
+++ App.js
@@ -4,27 +4,33 @@ import MainContent from "./components/MainContent"
 import Navbar from "./components/Navbar"
 
 function App() {
-    const date = new Date()
-    const hours = date.getHours()
-    let timeOfDay 
-    const styles = {
-        fontSize: "30px"}
-
-    if (hours < 12) {
-        timeOfDay = "morning"
-        styles.color = "#04756F"
-    } else if (hours >= 12 && hours < 17) {
-        timeOfDay = "afternoon"
-        styles.color = "#2E0927"
-    }else {
-        timeOfDay = "night"
-        styles.color = "#D90000"
-    }
 
     return (
-        <h1 style={styles}>Good {timeOfDay}!</h1>
+        <div className="todo-list">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        </div>
         // need to wrap in curly braces for the object and again for using JS
         // can't use background-color - needs to be camelCase
     )
 }
 export default App
+
+
+    // const date = new Date()
+    // const hours = date.getHours()
+    // let timeOfDay 
+    // const styles = {
+    //     fontSize: "30px"}
+
+    // if (hours < 12) {
+    //     timeOfDay = "morning"
+    //     styles.color = "#04756F"
+    // } else if (hours >= 12 && hours < 17) {
+    //     timeOfDay = "afternoon"
+    //     styles.color = "#2E0927"
+    // }else {
+    //     timeOfDay = "night"
+    //     styles.color = "#D90000"
+    // }
