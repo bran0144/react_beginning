dfc9463821b741a675ffb9397bd046ff3bebb78c
KatieGott <Katie.Gott@target.com>
2022-02-24 11:58:40 -0600

    using  map of array

--- JokeApp.js
+++ JokeApp.js
@@ -1,25 +1,15 @@
 import React from "react"
 import Joke from "./Joke.js"
+import jokesData from "./jokesData"
 
 
 function JokeApp() {
+
+    const jokeComponents= jokesData.map(joke => <Joke key={joke.id} question={joke.question} punchline={joke.punchline} />)
+        
     return (
-        <div className="jokes">
-            <Joke 
-                question="What do you call a fish wearing a bowtie?" 
-                punchline="Sofishticated."/>
-            <Joke 
-                question="If April showers bring May flowers, what do May flowers bring?"
-                punchline="Pilgrims."/>
-            <Joke 
-                question="dad joke"
-                punchline="ha ha"/>
-            <Joke 
-                question="dad joke"
-                punchline="ha ha"/>
-            <Joke 
-                question="dad joke"
-                punchline="ha ha"/>
+        <div>
+            {jokeComponents}
         </div>
     )
 }
---
+++ components/jokesData.js
@@ -0,0 +1,16 @@
+const jokesData = [
+    {
+    id: 1,
+    question: "What do you call a fish wearing a bowtie?", 
+    punchline: "Sofishticated."
+},
+{
+    id: 2,
+    question: "If April showers bring May flowers, what do May flowers bring?",
+    punchline: "Pilgrims."
+},
+{
+    id:3,
+    question: "dad joke",
+    punchline: "ha ha"}
+]
