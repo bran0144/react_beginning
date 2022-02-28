fcce6a6416857cc825a4f41532197fe4753c697a
KatieGott <Katie.Gott@target.com>
2022-02-28 13:28:18 -0600

    second practice with state and conditional

---
+++ StateApp.js
@@ -0,0 +1,25 @@
+import React, {Component} from "react"
+
+// Challenge:
+// Given an incomplete class-based component without a constructor, 
+// add a constructor and initialize state to fix the broken component.
+
+class StateApp extends Component {
+    constructor() {
+        super()
+        this.state = {
+            name: "Jane",
+            age: 12
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.name}</h1>
+                <h3>{this.state.age} years old</h3>
+            </div>
+    )    
+}
+}
+
+export default StateApp
