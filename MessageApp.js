21261b64d7035b2cb69adcc7ba68fa6b819b91ef
KatieGott <Katie.Gott@target.com>
2022-03-02 12:56:41 -0600

    conditional render exercise

---
+++ MessageApp.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react"
+import Conditional from "./Conditional"
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            unreadMessages: []
+        }
+    }
+    // &&
+    // true && false
+    render() {
+        return (
+            <div>
+                {this.state.unreadMessages.length > 0 && 
+                <h2>You have {this.state.unreadMessages.length} unread messages!</h2>}
+            </div>
+        )
+    }
+}
+
+export default App
