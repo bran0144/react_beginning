00a32b26ceeadf64ca16b49977a32a104a3f786a
KatieGott <Katie.Gott@target.com>
2022-03-02 11:59:00 -0600

    starting conditional rendering

---
+++ components/Conditional.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function Conditional(props) {
+    return <h1>Other stuff</h1>
+}
+    //old way to write it: 
+    // if(props.isLoading === true) {
+    //     return (
+    //         <h1>Loading...</h1>
+    // )
+    // }
+    // return (
+    //     <h1>Other stuff</h1>
+    //     )
+// }
+
+export default Conditional
